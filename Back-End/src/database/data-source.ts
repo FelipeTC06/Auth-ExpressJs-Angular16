7a9b0b27ceaba852698c3cc987d7791b6691d5e5
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2023-11-05 17:59:18 -0300

    refactor: config project

--- Back-End/src/database/data-source.ts
+++ Back-End/src/database/data-source.ts
@@ -1,17 +1,16 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    username: "admin",
+    password: "admin",
+    database: "auth-express-angular",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [],
     migrations: [],
     subscribers: [],
 })
