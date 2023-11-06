74739d44b3676fe9828b245d999f1fcb5d3a2455
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2023-11-06 17:09:31 -0300

    feat: add .env

--- Back-End/src/database/data-source.ts
+++ Back-End/src/database/data-source.ts
@@ -2,14 +2,16 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import User from "../app/entities/User"
 import { CreateUsersTable1674307725393 } from './migrations/1699217461224-CreateUsersTable'
+import * as dotenv  from "dotenv";
+dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "admin",
-    password: "admin",
-    database: "auth-express-angular",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: false,
     entities: [User],
